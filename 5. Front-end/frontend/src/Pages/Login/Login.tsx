5e29fea5738275e232fadf182c52f17b35721236
Leehzitha <leticia.burlinski@gmail.com>
2026-03-31 11:42:41 -0300

    Added galaxy theme

--- 5. Front-end/frontend/src/Pages/Login/Login.tsx
+++ 5. Front-end/frontend/src/Pages/Login/Login.tsx
@@ -2,6 +2,7 @@ import axios from "axios"
 import { useState } from "react"
 import Swal from "sweetalert2"
 import './Login.css';
+import bg from './../../assets/fundo.jpg';
 
 export const Login = () => {
     const [email, setEmail] = useState("")
@@ -30,14 +31,27 @@ export const Login = () => {
 
     return (
         <>
-            <div className="w-screen h-screen gap-10 bg-linear-to-t from-bg-primary via-bg-secondary from-10% via-30% to-60% to-bg-terciary flex-col flex justify-center items-center">
+            <div className="w-screen h-screen bg-cover bg-center flex flex-col justify-center items-center" 
+                style={{ backgroundImage: `url(${bg})` }} 
+            >
     
-                <p className="font-sans font-bold text-7xl">LOGIN</p>
-                <div className="bg-white shadow-[0px_40px_70px_rgba(0,0,0,0.1)] p-12 rounded-2xl flex flex-col gap-3.5">
+                <div 
+                    className="
+                    bg-white/0
+                    backdrop-blur-xl
+                    rounded-2xl
+                    p-10
+                    shadow-[0_30px_80px_rgba(0,0,0,0.6)]
+                    flex
+                    gap-5
+                    flex-col
+                    justify-center
+                    items-center">
 
-                    <input type="text" placeholder="Email" className="p-1.5 text-font font-sans tracking-widest w-100 bg-transparent border-0 border-b-2  border-gray-200 focus:border-bg-primary focus:outline-none transition"/>
-                    <input type="text" placeholder="Password" className="p-1.5 text-font font-sans tracking-widest  w-full bg-transparent border-0 border-b-2 border-gray-200 focus:border-bg-primary focus:outline-none transition" />
-                    <button className="bg-bg-primary mt-6 px-6 py-2 rounded-lg outline-none focus:outline-none active:scale-98 transition">Login</button>
+                    <p className="font-sans text-white font-bold text-5xl mb-5">Login</p>
+                    <input type="text" placeholder="Email" className="p-2.5 rounded-b-md text-title font-sans tracking-widest w-100 bg-input border-0  border-gray-200 focus:border-bg-primary focus:outline-none transition"/>
+                    <input type="text" placeholder="Password" className="p-2.5 rounded-b-md text-title font-sans tracking-widest w-100 bg-input border-0  border-gray-200 focus:border-bg-primary focus:outline-none transition" />
+                    <button className="bg-button w-full mt-6 px-6 py-2 rounded-lg outline-none focus:outline-none active:scale-98 transition">Login</button>
                 </div>
             </div>
         </>
